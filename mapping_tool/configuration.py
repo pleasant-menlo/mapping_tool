07332233320967f375f639fe43dad831b7dc0c75
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2025-07-21 11:59:43 -0400

    Harrison 2703 - EYEA/ETOB/SWYN - Query IMAP data access for pointing sets based on config

--- mapping_tool/configuration.py
+++ mapping_tool/configuration.py
@@ -3,6 +3,7 @@ import json
 from dataclasses import dataclass
 from typing import Optional
 
+import imap_data_access
 from imap_processing.ena_maps.utils.naming import MapDescriptor, MappableInstrumentShortName
 
 from pathlib import Path
@@ -89,3 +90,43 @@ class Configuration:
             spin_phase=spin_phase[self.spin_phase.lower()],
             coordinate_system=self.coordinate_system.lower()
         )
+
+
+def get_pointing_sets(descriptor: MapDescriptor, start_date: datetime.datetime, end_date: datetime.datetime):
+    map_instrument_pset_descriptors = []
+
+    if descriptor.instrument == MappableInstrumentShortName.HI:
+        if descriptor.sensor in ["45", "combined"]:
+            map_instrument_pset_descriptors.append(f"45sensor-pset")
+        if descriptor.sensor in ["90", "combined"]:
+            map_instrument_pset_descriptors.append(f"90sensor-pset")
+
+    elif descriptor.instrument == MappableInstrumentShortName.LO:
+        map_instrument_pset_descriptors.append("pset")
+
+    elif descriptor.instrument == MappableInstrumentShortName.ULTRA:
+        pset_string = "spacecraftpset" if descriptor.frame_descriptor == "sf" else "heliopset"
+        if descriptor.sensor in ["45", "combined"]:
+            map_instrument_pset_descriptors.append(f"45sensor-{pset_string}")
+        if descriptor.sensor in ["90", "combined"]:
+            map_instrument_pset_descriptors.append(f"90sensor-{pset_string}")
+
+    assert len(map_instrument_pset_descriptors) > 0
+    instrument_for_query = descriptor.instrument.name.lower()
+    start_date = start_date.strftime("%Y%m%d")
+    end_date = end_date.strftime("%Y%m%d")
+
+    files = []
+    for pset_descriptor in map_instrument_pset_descriptors:
+        files.extend(imap_data_access.query(instrument=instrument_for_query,
+                                            start_date=start_date, end_date=end_date,
+                                            data_level="l1c", descriptor=pset_descriptor))
+
+    if descriptor.survival_corrected == "sp":
+        files.extend(imap_data_access.query(instrument="glows",
+                                            start_date=start_date,
+                                            end_date=end_date,
+                                            data_level="l3e",
+                                            descriptor=f"survival-probability-{instrument_for_query[:2]}"))
+
+    return [pset['file_path'] for pset in files]
--- test/test_configuration.py
+++ test/test_configuration.py
@@ -1,14 +1,15 @@
 import datetime
+from datetime import datetime
 from copy import deepcopy
 
 import jsonschema.exceptions
 from pathlib import Path
 from typing import Dict
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, call
 
 from mapping_tool import config_schema
-from mapping_tool.configuration import Configuration, CanonicalMapPeriod
+from mapping_tool.configuration import Configuration, CanonicalMapPeriod, get_pointing_sets
 from test.test_builders import create_configuration, create_config_dict, create_cannonical_map_period
 from test.test_helpers import get_example_config_path
 from imap_processing.ena_maps.utils.naming import MapDescriptor, MappableInstrumentShortName
@@ -215,24 +216,144 @@ class TestConfiguration(TestCase):
                 self.assertEqual(sensor, descriptor.sensor)
                 self.assertEqual(instrument_descriptor, descriptor.instrument_descriptor)
 
+    @patch('mapping_tool.configuration.imap_data_access.query')
+    def test_get_pointing_sets(self, mock_query):
+        expected_pointing_sets = ["pset_1", "pset_2", "pset_3"]
+        mock_query.return_value = [{"file_path": file_name} for file_name in expected_pointing_sets]
+
+        start_date = datetime(2025, 1, 1)
+        end_date = datetime(2025, 2, 1)
+        cases = [
+            ("sf", MappableInstrumentShortName.HI, "90", "90sensor-pset"),
+            ("sf", MappableInstrumentShortName.HI, "45", "45sensor-pset"),
+            ("sf", MappableInstrumentShortName.LO, "90", "pset"),
+            ("sf", MappableInstrumentShortName.ULTRA, "90", "90sensor-spacecraftpset"),
+            ("sf", MappableInstrumentShortName.ULTRA, "45", "45sensor-spacecraftpset"),
+            ("hf", MappableInstrumentShortName.ULTRA, "90", "90sensor-heliopset"),
+            ("hf", MappableInstrumentShortName.ULTRA, "45", "45sensor-heliopset"),
+        ]
+
+        for frame_descriptor, instrument, sensor, expected_descriptor in cases:
+            with self.subTest(f"{frame_descriptor} {instrument} {sensor}"):
+                mock_query.reset_mock()
+                descriptor = MapDescriptor(
+                    frame_descriptor=frame_descriptor,
+                    resolution_str="2deg",
+                    duration=2,
+                    instrument=instrument,
+                    sensor=sensor,
+                    principal_data="ena",
+                    species='h',
+                    survival_corrected="nsp",
+                    spin_phase="ram",
+                    coordinate_system="hae"
+                )
+
+                pointing_sets = get_pointing_sets(descriptor, start_date, end_date)
+
+                mock_query.assert_called_once_with(
+                    instrument=instrument.name.lower(),
+                    start_date=start_date.strftime("%Y%m%d"),
+                    end_date=end_date.strftime("%Y%m%d"),
+                    data_level="l1c",
+                    descriptor=expected_descriptor
+                )
+                self.assertEqual(expected_pointing_sets, pointing_sets)
+
+    @patch('mapping_tool.configuration.imap_data_access.query')
+    def test_get_pointing_sets_for_ultra_combined(self, mock_query):
+        expected_pointing_sets = ["u45-pset1", "u45-pset2", "u90-pset1", "u90-pset2", "glows_ultra_pset1",
+                                  "glows_ultra_pset2"]
+        mock_query.side_effect = [
+            [{"file_path": "u45-pset1"}, {"file_path": "u45-pset2"}],
+            [{"file_path": "u90-pset1"}, {"file_path": "u90-pset2"}],
+            [{"file_path": "glows_ultra_pset1"}, {"file_path": "glows_ultra_pset2"}]
+        ]
+
+        descriptor = MapDescriptor(
+            frame_descriptor="sf",
+            resolution_str="nside2",
+            duration=2,
+            instrument=MappableInstrumentShortName.ULTRA,
+            sensor="combined",
+            principal_data="ena",
+            species='h',
+            survival_corrected="sp",
+            spin_phase="ram",
+            coordinate_system="hae"
+        )
+
+        pointing_sets = get_pointing_sets(descriptor, start_date=datetime(2025, 1, 1), end_date=datetime(2025, 2, 1))
+
+        mock_query.assert_has_calls([
+            call(instrument="ultra", data_level="l1c", descriptor="45sensor-spacecraftpset", start_date="20250101",
+                 end_date="20250201"),
+            call(instrument="ultra", data_level="l1c", descriptor="90sensor-spacecraftpset", start_date="20250101",
+                 end_date="20250201"),
+            call(instrument="glows", data_level="l3e", descriptor="survival-probability-ul", start_date="20250101",
+                 end_date="20250201")
+        ])
+
+        self.assertEqual(expected_pointing_sets, pointing_sets)
+
+    @patch('mapping_tool.configuration.imap_data_access.query')
+    def test_get_pointing_sets_for_hi_combined(self, mock_query):
+        expected_pointing_sets = [
+            "h45-pset1", "h45-pset2",
+            "h90-pset1", "h90-pset2",
+            "glows_hi_pset1", "glows_hi_pset2",
+        ]
+
+        mock_query.side_effect = [
+            [{"file_path": "h45-pset1"}, {"file_path": "h45-pset2"}],
+            [{"file_path": "h90-pset1"}, {"file_path": "h90-pset2"}],
+            [{"file_path": "glows_hi_pset1"}, {"file_path": "glows_hi_pset2"}]
+        ]
+
+        descriptor = MapDescriptor(
+            frame_descriptor="sf",
+            resolution_str="nside2",
+            duration=2,
+            instrument=MappableInstrumentShortName.HI,
+            sensor="combined",
+            principal_data="ena",
+            species='h',
+            survival_corrected="sp",
+            spin_phase="ram",
+            coordinate_system="hae"
+        )
+
+        pointing_sets = get_pointing_sets(descriptor, start_date=datetime(2025, 1, 1), end_date=datetime(2025, 2, 1))
+
+        mock_query.assert_has_calls([
+            call(instrument="hi", data_level="l1c", descriptor="45sensor-pset", start_date="20250101",
+                 end_date="20250201"),
+            call(instrument="hi", data_level="l1c", descriptor="90sensor-pset", start_date="20250101",
+                 end_date="20250201"),
+            call(instrument="glows", data_level="l3e", descriptor="survival-probability-hi", start_date="20250101",
+                 end_date="20250201")
+        ])
+
+        self.assertEqual(expected_pointing_sets, pointing_sets)
+
 
 class TestCanonicalMapPeriod(TestCase):
     def test_calculate_date_range(self):
         cases = [
-            (2010, 1, 3, (datetime.datetime(2010, 1, 1, 0, 0), datetime.datetime(2010, 4, 2, 7, 30))),
-            (2012, 2, 3, (datetime.datetime(2012, 4, 1, 7, 30), datetime.datetime(2012, 7, 1, 15, 0))),
-            (2013, 3, 3, (datetime.datetime(2013, 7, 2, 15, 0), datetime.datetime(2013, 10, 1, 22, 30))),
-            (2017, 4, 3, (datetime.datetime(2017, 10, 1, 22, 30), datetime.datetime(2018, 1, 1, 6, 0))),
+            (2010, 1, 3, (datetime(2010, 1, 1, 0, 0), datetime(2010, 4, 2, 7, 30))),
+            (2012, 2, 3, (datetime(2012, 4, 1, 7, 30), datetime(2012, 7, 1, 15, 0))),
+            (2013, 3, 3, (datetime(2013, 7, 2, 15, 0), datetime(2013, 10, 1, 22, 30))),
+            (2017, 4, 3, (datetime(2017, 10, 1, 22, 30), datetime(2018, 1, 1, 6, 0))),
 
-            (2010, 1, 6, (datetime.datetime(2010, 1, 1, 0, 0), datetime.datetime(2010, 7, 2, 15, 0))),
-            (2012, 2, 6, (datetime.datetime(2012, 4, 1, 7, 30), datetime.datetime(2012, 9, 30, 22, 30))),
-            (2013, 3, 6, (datetime.datetime(2013, 7, 2, 15, 0), datetime.datetime(2014, 1, 1, 6, 0))),
-            (2017, 4, 6, (datetime.datetime(2017, 10, 1, 22, 30), datetime.datetime(2018, 4, 2, 13, 30))),
+            (2010, 1, 6, (datetime(2010, 1, 1, 0, 0), datetime(2010, 7, 2, 15, 0))),
+            (2012, 2, 6, (datetime(2012, 4, 1, 7, 30), datetime(2012, 9, 30, 22, 30))),
+            (2013, 3, 6, (datetime(2013, 7, 2, 15, 0), datetime(2014, 1, 1, 6, 0))),
+            (2017, 4, 6, (datetime(2017, 10, 1, 22, 30), datetime(2018, 4, 2, 13, 30))),
 
-            (2010, 1, 12, (datetime.datetime(2010, 1, 1, 0, 0), datetime.datetime(2011, 1, 1, 6, 0))),
-            (2012, 2, 12, (datetime.datetime(2012, 4, 1, 7, 30), datetime.datetime(2013, 4, 1, 13, 30))),
-            (2013, 3, 12, (datetime.datetime(2013, 7, 2, 15, 0), datetime.datetime(2014, 7, 2, 21, 0))),
-            (2017, 4, 12, (datetime.datetime(2017, 10, 1, 22, 30), datetime.datetime(2018, 10, 2, 4, 30))),
+            (2010, 1, 12, (datetime(2010, 1, 1, 0, 0), datetime(2011, 1, 1, 6, 0))),
+            (2012, 2, 12, (datetime(2012, 4, 1, 7, 30), datetime(2013, 4, 1, 13, 30))),
+            (2013, 3, 12, (datetime(2013, 7, 2, 15, 0), datetime(2014, 7, 2, 21, 0))),
+            (2017, 4, 12, (datetime(2017, 10, 1, 22, 30), datetime(2018, 10, 2, 4, 30))),
         ]
         for year, quarter, period, expected in cases:
             with self.subTest(f"year: {year}, quarter: {quarter}, period: {period}"):
