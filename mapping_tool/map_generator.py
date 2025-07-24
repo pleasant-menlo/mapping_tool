720150f585dabc0460d3683f8b7dd967da46fa47
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2025-07-24 10:55:41 -0400

    Harrison 2704 - EYEA/SWYN - Add tests for file write/overwrite behavior

--- mapping_tool/map_generator.py
+++ mapping_tool/map_generator.py
@@ -10,23 +10,28 @@ from mapping_tool.configuration import Configuration
 def process(processor: ProcessInstrument, config: Configuration) -> list[Path]:
     downloaded_deps = processor.pre_processing()
     results = processor.do_processing(downloaded_deps)
+
+    files_written = []
+
     with tempfile.TemporaryDirectory() as temp_dir:
         old_data_dir = imap_data_access.config["DATA_DIR"]
         try:
             imap_data_access.config["DATA_DIR"] = Path(temp_dir)
             processor.post_processing(results, downloaded_deps)
-            files = Path(temp_dir).rglob("*.cdf")
+            files = list(Path(temp_dir).rglob("*.cdf"))
             for file in files:
-                if (config.output_directory / file.name).exists():
+                output_file_path = config.output_directory / file.name
+                if output_file_path.exists():
                     overwrite = input(f"File {file.name} already exists. Would you like to overwrite it? (Y/n) ")
                     if overwrite not in ["Y", "y", ""]:
                         continue
 
-                print("Writing to: ", config.output_directory / file.name)
-                file.replace(config.output_directory / file.name)
+                print("Writing to: ", output_file_path)
+                file.replace(output_file_path)
+                files_written.append(output_file_path)
         except Exception as e:
             raise e
         finally:
             imap_data_access.config["DATA_DIR"] = old_data_dir
     processor.cleanup()
-    return files
+    return files_written
--- test/test_map_generator.py
+++ test/test_map_generator.py
@@ -1,5 +1,9 @@
+import tempfile
 import unittest
-from unittest.mock import Mock
+from pathlib import Path
+from unittest.mock import Mock, patch, call
+
+import imap_data_access
 
 from mapping_tool.configuration import Configuration
 from mapping_tool.map_generator import process
@@ -21,3 +25,49 @@ class TestMapGenerator(unittest.TestCase):
         do_processing.assert_called_once_with(pre_processing.return_value)
         post_processing.assert_called_once_with(do_processing.return_value, pre_processing.return_value)
         mock_processor.cleanup.assert_called_once()
+
+    @patch('mapping_tool.map_generator.input')
+    def test_process_prompts_user_to_overwrite_already_existing_file(self, mock_input):
+        mock_processor = Mock()
+
+        config: Configuration = Mock(spec=Configuration)
+
+        hi_map_that_gets_overwritten = "hi_map.cdf"
+        ultra_map_that_is_not_overwritten = "ultra_map.cdf"
+        some_new_map = "some_new_map.cdf"
+
+        original_data_dir = imap_data_access.config['DATA_DIR']
+
+        def write_cdf_files_to_data_dir(results, downloaded_deps):
+            self.assertNotEqual(imap_data_access.config["DATA_DIR"], original_data_dir)
+            (imap_data_access.config["DATA_DIR"] / hi_map_that_gets_overwritten).write_text("new hi map data")
+            (imap_data_access.config["DATA_DIR"] / ultra_map_that_is_not_overwritten).write_text("should be ignored")
+            (imap_data_access.config["DATA_DIR"] / some_new_map).write_text("brand new map")
+
+        mock_processor.post_processing.side_effect = write_cdf_files_to_data_dir
+
+        mock_input.side_effect = ["y", "n"]
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            output_directory = Path(temp_dir)
+            (output_directory / hi_map_that_gets_overwritten).write_text("old hi map data")
+            (output_directory / ultra_map_that_is_not_overwritten).write_text("ultra data")
+
+            config.output_directory = output_directory
+
+            output_cdfs = process(mock_processor, config)
+
+            mock_input.assert_has_calls([
+                call(f"File {hi_map_that_gets_overwritten} already exists. Would you like to overwrite it? (Y/n) "),
+                call(f"File {ultra_map_that_is_not_overwritten} already exists. Would you like to overwrite it? (Y/n) ")
+            ])
+
+            self.assertEqual("new hi map data", (output_directory / hi_map_that_gets_overwritten).read_text())
+            self.assertEqual("ultra data", (output_directory / ultra_map_that_is_not_overwritten).read_text())
+            self.assertEqual("brand new map", (output_directory / some_new_map).read_text())
+
+            self.assertEqual(original_data_dir, imap_data_access.config["DATA_DIR"])
+
+            expected_output_files = [output_directory / hi_map_that_gets_overwritten,
+                                     output_directory / some_new_map]
+            self.assertEqual(expected_output_files, output_cdfs)
