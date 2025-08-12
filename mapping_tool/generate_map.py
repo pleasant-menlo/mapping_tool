55395be41678e07e84b4f8c877f0d21e67e99265
Petty Dev Tigris <akaumeheiwa@menloinnovations.com>
2025-08-12 13:55:36 -0400

    Harrison 2709 - PMCL/EYEA - Create input maps for an L3 map by instrument rather than generically for all L3 maps

--- mapping_tool/generate_map.py
+++ mapping_tool/generate_map.py
@@ -21,8 +21,20 @@ import spiceypy
 
 from mapping_tool.mapping_tool_descriptor import MappingToolDescriptor
 
+logger = logging.getLogger(__name__)
 
 def get_dependencies_for_l3_map(map_descriptor: MappingToolDescriptor) -> list[MappingToolDescriptor]:
+    match map_descriptor.instrument:
+        case MappableInstrumentShortName.HI:
+            return get_dependencies_for_hi_l3_map(map_descriptor)
+        case MappableInstrumentShortName.ULTRA:
+            return get_dependencies_for_ultra_l3_map(map_descriptor)
+        case MappableInstrumentShortName.LO:
+            return get_dependencies_for_lo_l3_map(map_descriptor)
+        case _:
+            raise ValueError("Don't know correct dependencies for this specific instrument", map_descriptor.instrument)
+
+def get_dependencies_for_hi_l3_map(map_descriptor: MappingToolDescriptor) -> list[MappingToolDescriptor]:
     match map_descriptor:
         case MapDescriptor(principal_data="spx"):
             return [replace(map_descriptor, principal_data="ena")]
@@ -46,7 +58,28 @@ def get_dependencies_for_l3_map(map_descriptor: MappingToolDescriptor) -> list[M
         case MapDescriptor(survival_corrected="sp", spin_phase="ram" | "anti"):
             return [replace(map_descriptor, survival_corrected="nsp")]
         case _:
-            return []
+            raise ValueError("Don't know correct dependencies for", map_descriptor)
+
+def get_dependencies_for_ultra_l3_map(map_descriptor: MappingToolDescriptor) -> list[MappingToolDescriptor]:
+    match map_descriptor:
+        case MapDescriptor(principal_data="spx"):
+            return [replace(map_descriptor, principal_data="ena")]
+        case MapDescriptor(sensor="combined"):
+            return [replace(map_descriptor, sensor="45"), replace(map_descriptor, sensor="90")]
+        case MapDescriptor(sensor="90"|"45", survival_corrected="sp"):
+            return [replace(map_descriptor, survival_corrected="nsp")]
+        case _:
+            raise ValueError("Don't know correct dependencies for", map_descriptor)
+
+
+def get_dependencies_for_lo_l3_map(map_descriptor: MappingToolDescriptor) -> list[MappingToolDescriptor]:
+    match map_descriptor:
+        case MapDescriptor(principal_data="spx"):
+            return [replace(map_descriptor, principal_data="ena")]
+        case MapDescriptor(survival_corrected="sp"):
+            return [replace(map_descriptor, survival_corrected="nsp")]
+        case _:
+            raise ValueError("Don't know correct dependencies for", map_descriptor)
 
 
 def get_data_level_for_descriptor(descriptor: MappingToolDescriptor):
@@ -59,13 +92,19 @@ def get_data_level_for_descriptor(descriptor: MappingToolDescriptor):
 
 
 def generate_map(descriptor: MappingToolDescriptor, start: datetime, end: datetime) -> Path:
+    logger.info("preparing to generate map %s", descriptor.to_mapping_tool_string())
     data_level = get_data_level_for_descriptor(descriptor)
     if data_level == DataLevel.L2:
+        logger.info("generating l2 map %s", descriptor.to_mapping_tool_string())
         return generate_l2_map(descriptor, start, end)
     elif data_level == DataLevel.L3:
         map_deps = []
-        for dependency in get_dependencies_for_l3_map(descriptor):
+        deps = get_dependencies_for_l3_map(descriptor)
+        logger.info("identified dependencies %s", deps)
+        for dependency in deps:
+            logger.info("generating dependency %s", dependency.to_mapping_tool_string())
             map_deps.append(generate_map(dependency, start, end))
+        logger.info("generating l3 map %s", descriptor.to_mapping_tool_string())
         return generate_l3_map(descriptor, start, end, map_deps)
     else:
         raise ValueError(f"Cannot produce map for instrument: {descriptor.instrument_descriptor}")
