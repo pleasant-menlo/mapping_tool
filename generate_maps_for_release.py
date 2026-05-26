8573b988e10fe151b9572254ccc839e981ef2d19
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2026-05-26 16:49:42 -0400

    Harrison 3232 - SWYN/EYEA - Add new Hi maps to the script to generate all maps for release, and automatically rename maps to have -INITIAL to indicate these are first data release maps

--- generate_maps_for_release.py
+++ generate_maps_for_release.py
@@ -13,6 +13,8 @@ from imap_data_access.processing_input import ScienceFilePath, ScienceInput
 from mapping_tool.generate_map import generate_map
 import imap_data_access
 
+from spacepy.pycdf import CDF
+
 MAPS_TO_GENERATE = [
     {
         "start": datetime(2025, 11, 15, tzinfo=timezone.utc),
@@ -30,7 +32,7 @@ MAPS_TO_GENERATE = [
             "ulc-spx-h-hf-sp-full-hae-6deg-6mo",
             "u90-spx-h-hf-sp-full-hae-6deg-6mo",
             "u45-spx-h-hf-sp-full-hae-6deg-6mo",
-        ]
+        ],
     },
     {
         "start": datetime(2025, 12, 3),
@@ -40,13 +42,11 @@ MAPS_TO_GENERATE = [
             "h45-ena-h-hf-nsp-full-hae-4deg-6mo",
             "h45-spx-h-hf-sp-full-hae-6deg-6mo",
             "h45-spx-h-hf-sp-full-hae-4deg-6mo",
-
-            # not in release but needed for combined maps
-            'h45-ena-h-hf-sp-ram-hae-6deg-6mo',
-            'h45-ena-h-hf-sp-anti-hae-6deg-6mo',
-            'h45-ena-h-hf-sp-ram-hae-4deg-6mo',
-            'h45-ena-h-hf-sp-anti-hae-4deg-6mo',
-        ]
+            "h45-ena-h-hf-sp-ram-hae-6deg-6mo",
+            "h45-ena-h-hf-sp-anti-hae-6deg-6mo",
+            "h45-ena-h-hf-sp-ram-hae-4deg-6mo",
+            "h45-ena-h-hf-sp-anti-hae-4deg-6mo",
+        ],
     },
     {
         "start": datetime(2025, 11, 16),
@@ -56,36 +56,58 @@ MAPS_TO_GENERATE = [
             "h90-ena-h-hf-nsp-full-hae-4deg-6mo",
             "h90-spx-h-hf-sp-full-hae-6deg-6mo",
             "h90-spx-h-hf-sp-full-hae-4deg-6mo",
-
-            # not in release but needed for combined maps
-            'h90-ena-h-hf-sp-ram-hae-6deg-6mo',
-            'h90-ena-h-hf-sp-anti-hae-6deg-6mo',
-            'h90-ena-h-hf-sp-ram-hae-4deg-6mo',
-            'h90-ena-h-hf-sp-anti-hae-4deg-6mo',
-        ]
-    }
+            "h90-ena-h-hf-sp-ram-hae-6deg-6mo",
+            "h90-ena-h-hf-sp-anti-hae-6deg-6mo",
+            "h90-ena-h-hf-sp-ram-hae-4deg-6mo",
+            "h90-ena-h-hf-sp-anti-hae-4deg-6mo",
+        ],
+    },
+    {
+        "start": datetime(2025, 12, 3),
+        "end": datetime(2026, 5, 17),
+        "descriptors": [
+            "h45-ena-h-sf-nsp-full-hae-6deg-6mo",
+            "h45-ena-h-sf-nsp-ram-hae-6deg-6mo",
+            "h45-ena-h-sf-nsp-anti-hae-6deg-6mo",
+            "h45-ena-h-sf-nsp-full-hae-4deg-6mo",
+            "h45-ena-h-sf-nsp-ram-hae-4deg-6mo",
+            "h45-ena-h-sf-nsp-anti-hae-4deg-6mo",
+        ],
+    },
+    {
+        "start": datetime(2025, 11, 16),
+        "end": datetime(2026, 5, 17),
+        "descriptors": [
+            "h90-ena-h-sf-nsp-full-hae-6deg-6mo",
+            "h90-ena-h-sf-nsp-ram-hae-6deg-6mo",
+            "h90-ena-h-sf-nsp-anti-hae-6deg-6mo",
+            "h90-ena-h-sf-nsp-full-hae-4deg-6mo",
+            "h90-ena-h-sf-nsp-ram-hae-4deg-6mo",
+            "h90-ena-h-sf-nsp-anti-hae-4deg-6mo",
+        ],
+    },
 ]
 
 
 COMBINED_MAPS_TO_GENERATE = [
     {
-        'descriptor': 'hic-ena-h-hf-sp-full-hae-6deg-6mo',
-        'deps': [
-            'h45-ena-h-hf-sp-ram-hae-6deg-6mo',
-            'h45-ena-h-hf-sp-anti-hae-6deg-6mo',
-            'h90-ena-h-hf-sp-ram-hae-6deg-6mo',
-            'h90-ena-h-hf-sp-anti-hae-6deg-6mo',
-        ]
+        "descriptor": "hic-ena-h-hf-sp-full-hae-6deg-6mo",
+        "deps": [
+            "h45-ena-h-hf-sp-ram-hae-6deg-6mo",
+            "h45-ena-h-hf-sp-anti-hae-6deg-6mo",
+            "h90-ena-h-hf-sp-ram-hae-6deg-6mo",
+            "h90-ena-h-hf-sp-anti-hae-6deg-6mo",
+        ],
     },
     {
-        'descriptor': 'hic-ena-h-hf-sp-full-hae-4deg-6mo',
-        'deps': [
-            'h45-ena-h-hf-sp-ram-hae-4deg-6mo',
-            'h45-ena-h-hf-sp-anti-hae-4deg-6mo',
-            'h90-ena-h-hf-sp-ram-hae-4deg-6mo',
-            'h90-ena-h-hf-sp-anti-hae-4deg-6mo',
-        ]
-    }
+        "descriptor": "hic-ena-h-hf-sp-full-hae-4deg-6mo",
+        "deps": [
+            "h45-ena-h-hf-sp-ram-hae-4deg-6mo",
+            "h45-ena-h-hf-sp-anti-hae-4deg-6mo",
+            "h90-ena-h-hf-sp-ram-hae-4deg-6mo",
+            "h90-ena-h-hf-sp-anti-hae-4deg-6mo",
+        ],
+    },
 ]
 
 if __name__ == "__main__":
@@ -101,7 +123,7 @@ if __name__ == "__main__":
     for map_set_to_generate in MAPS_TO_GENERATE:
         time_range = (
             map_set_to_generate["start"].replace(tzinfo=timezone.utc),
-            map_set_to_generate["end"].replace(tzinfo=timezone.utc)
+            map_set_to_generate["end"].replace(tzinfo=timezone.utc),
         )
 
         for map_descriptor in map_set_to_generate["descriptors"]:
@@ -114,17 +136,19 @@ if __name__ == "__main__":
             generate_map(dependencies, generated_maps)
 
     for combined_map in COMBINED_MAPS_TO_GENERATE:
-        combined_descriptor = str(combined_map['descriptor'])
+        combined_descriptor = str(combined_map["descriptor"])
 
         if combined_descriptor not in generated_maps:
-            dependencies = ProcessingInputCollection(*[ScienceInput(generated_maps[dep].name) for dep in combined_map['deps']])
+            dependencies = ProcessingInputCollection(
+                *[ScienceInput(generated_maps[dep].name) for dep in combined_map["deps"]]
+            )
             combined_metadata = InputMetadata(
-                instrument='hi',
-                data_level='l3',
+                instrument="hi",
+                data_level="l3",
                 start_date=datetime(2025, 11, 16),
                 end_date=datetime(2026, 5, 17),
-                version='v000',
-                descriptor=combined_map['descriptor'],
+                version="v000",
+                descriptor=combined_map["descriptor"],
             )
             [combined_output_path] = HiProcessor(dependencies, combined_metadata).process()
             generated_maps[combined_descriptor] = combined_output_path
@@ -133,74 +157,111 @@ if __name__ == "__main__":
         if spx_descriptor not in generated_maps:
             spx_dependencies = ProcessingInputCollection(ScienceInput(combined_output_path.name))
             spx_metadata = InputMetadata(
-                instrument='hi',
-                data_level='l3',
+                instrument="hi",
+                data_level="l3",
                 start_date=datetime(2025, 11, 16),
                 end_date=datetime(2026, 5, 17),
-                version='v000',
+                version="v000",
                 descriptor=spx_descriptor,
             )
             [spx_output_path] = HiProcessor(spx_dependencies, spx_metadata).process()
             generated_maps[spx_descriptor] = spx_output_path
 
-    # MAPS_TO_RELEASE = [
-    #     "h45-ena-h-hf-nsp-full-hae-6deg-6mo",
-    #     "h45-ena-h-hf-nsp-ram-hae-6deg-6mo",
-    #     "h45-ena-h-hf-nsp-anti-hae-6deg-6mo",
-    #     "h90-ena-h-hf-nsp-full-hae-6deg-6mo",
-    #     "h90-ena-h-hf-nsp-ram-hae-6deg-6mo",
-    #     "h90-ena-h-hf-nsp-anti-hae-6deg-6mo",
-    #     "h45-ena-h-hf-nsp-full-hae-4deg-6mo",
-    #     "h45-ena-h-hf-nsp-ram-hae-4deg-6mo",
-    #     "h45-ena-h-hf-nsp-anti-hae-4deg-6mo",
-    #     "h90-ena-h-hf-nsp-full-hae-4deg-6mo",
-    #     "h90-ena-h-hf-nsp-ram-hae-4deg-6mo",
-    #     "h90-ena-h-hf-nsp-anti-hae-4deg-6mo",
-    #     "h90-ena-h-hf-sp-full-hae-6deg-6mo",
-    #     "h45-ena-h-hf-sp-full-hae-6deg-6mo",
-    #     "h90-ena-h-hf-sp-full-hae-4deg-6mo",
-    #     "h45-ena-h-hf-sp-full-hae-4deg-6mo",
-    #     "h45-spx-h-hf-sp-full-hae-6deg-6mo",
-    #     "h90-spx-h-hf-sp-full-hae-6deg-6mo",
-    #     "h45-spx-h-hf-sp-full-hae-4deg-6mo",
-    #     "h90-spx-h-hf-sp-full-hae-4deg-6mo",
-    #     "hic-ena-h-hf-sp-full-hae-6deg-6mo",
-    #     "hic-ena-h-hf-sp-full-hae-4deg-6mo",
-    #     "hic-spx-h-hf-sp-full-hae-6deg-6mo",
-    #     "hic-spx-h-hf-sp-full-hae-4deg-6mo",
-    #     "u90-ena-h-hf-nsp-full-hae-4deg-6mo",
-    #     "u45-ena-h-hf-nsp-full-hae-4deg-6mo",
-    #     "u90-ena-h-hf-nsp-full-hae-6deg-6mo",
-    #     "u45-ena-h-hf-nsp-full-hae-6deg-6mo",
-    #     "u90-ena-h-sf-nsp-full-hae-4deg-6mo",
-    #     "u45-ena-h-sf-nsp-full-hae-4deg-6mo",
-    #     "u90-ena-h-sf-nsp-full-hae-6deg-6mo",
-    #     "u45-ena-h-sf-nsp-full-hae-6deg-6mo",
-    #     "ulc-ena-h-hf-sp-full-hae-4deg-6mo",
-    #     "ulc-ena-h-hf-sp-full-hae-6deg-6mo",
-    #     "ulc-ena-h-hf-nsp-full-hae-4deg-6mo",
-    #     "ulc-ena-h-hf-nsp-full-hae-6deg-6mo",
-    #     "u90-ena-h-hf-sp-full-hae-4deg-6mo",
-    #     "u90-ena-h-sf-sp-full-hae-4deg-6mo",
-    #     "u90-ena-h-hf-sp-full-hae-6deg-6mo",
-    #     "u90-ena-h-sf-sp-full-hae-6deg-6mo",
-    #     "u45-ena-h-hf-sp-full-hae-4deg-6mo",
-    #     "u45-ena-h-sf-sp-full-hae-4deg-6mo",
-    #     "u45-ena-h-hf-sp-full-hae-6deg-6mo",
-    #     "u45-ena-h-sf-sp-full-hae-6deg-6mo",
-    #     "ulc-spx-h-hf-sp-full-hae-4deg-6mo",
-    #     "u90-spx-h-hf-sp-full-hae-4deg-6mo",
-    #     "u45-spx-h-hf-sp-full-hae-4deg-6mo",
-    #     "ulc-spx-h-hf-sp-full-hae-6deg-6mo",
-    #     "u90-spx-h-hf-sp-full-hae-6deg-6mo",
-    #     "u45-spx-h-hf-sp-full-hae-6deg-6mo",
-    # ]
+    MAPS_TO_RELEASE = [
+        "h45-ena-h-hf-nsp-full-hae-6deg-6mo",
+        "h45-ena-h-hf-nsp-ram-hae-6deg-6mo",
+        "h45-ena-h-hf-nsp-anti-hae-6deg-6mo",
+        "h90-ena-h-hf-nsp-full-hae-6deg-6mo",
+        "h90-ena-h-hf-nsp-ram-hae-6deg-6mo",
+        "h90-ena-h-hf-nsp-anti-hae-6deg-6mo",
+        "h45-ena-h-hf-nsp-full-hae-4deg-6mo",
+        "h45-ena-h-hf-nsp-ram-hae-4deg-6mo",
+        "h45-ena-h-hf-nsp-anti-hae-4deg-6mo",
+        "h90-ena-h-hf-nsp-full-hae-4deg-6mo",
+        "h90-ena-h-hf-nsp-ram-hae-4deg-6mo",
+        "h90-ena-h-hf-nsp-anti-hae-4deg-6mo",
+        "h90-ena-h-hf-sp-full-hae-6deg-6mo",
+        "h45-ena-h-hf-sp-full-hae-6deg-6mo",
+        "h90-ena-h-hf-sp-full-hae-4deg-6mo",
+        "h45-ena-h-hf-sp-full-hae-4deg-6mo",
+        "h45-spx-h-hf-sp-full-hae-6deg-6mo",
+        "h90-spx-h-hf-sp-full-hae-6deg-6mo",
+        "h45-spx-h-hf-sp-full-hae-4deg-6mo",
+        "h90-spx-h-hf-sp-full-hae-4deg-6mo",
+        "hic-ena-h-hf-sp-full-hae-6deg-6mo",
+        "hic-ena-h-hf-sp-full-hae-4deg-6mo",
+        "hic-spx-h-hf-sp-full-hae-6deg-6mo",
+        "hic-spx-h-hf-sp-full-hae-4deg-6mo",
+        "u90-ena-h-hf-nsp-full-hae-4deg-6mo",
+        "u45-ena-h-hf-nsp-full-hae-4deg-6mo",
+        "u90-ena-h-hf-nsp-full-hae-6deg-6mo",
+        "u45-ena-h-hf-nsp-full-hae-6deg-6mo",
+        "u90-ena-h-sf-nsp-full-hae-4deg-6mo",
+        "u45-ena-h-sf-nsp-full-hae-4deg-6mo",
+        "u90-ena-h-sf-nsp-full-hae-6deg-6mo",
+        "u45-ena-h-sf-nsp-full-hae-6deg-6mo",
+        "ulc-ena-h-hf-sp-full-hae-4deg-6mo",
+        "ulc-ena-h-hf-sp-full-hae-6deg-6mo",
+        "ulc-ena-h-hf-nsp-full-hae-4deg-6mo",
+        "ulc-ena-h-hf-nsp-full-hae-6deg-6mo",
+        "u90-ena-h-hf-sp-full-hae-4deg-6mo",
+        "u90-ena-h-sf-sp-full-hae-4deg-6mo",
+        "u90-ena-h-hf-sp-full-hae-6deg-6mo",
+        "u90-ena-h-sf-sp-full-hae-6deg-6mo",
+        "u45-ena-h-hf-sp-full-hae-4deg-6mo",
+        "u45-ena-h-sf-sp-full-hae-4deg-6mo",
+        "u45-ena-h-hf-sp-full-hae-6deg-6mo",
+        "u45-ena-h-sf-sp-full-hae-6deg-6mo",
+        "ulc-spx-h-hf-sp-full-hae-4deg-6mo",
+        "u90-spx-h-hf-sp-full-hae-4deg-6mo",
+        "u45-spx-h-hf-sp-full-hae-4deg-6mo",
+        "ulc-spx-h-hf-sp-full-hae-6deg-6mo",
+        "u90-spx-h-hf-sp-full-hae-6deg-6mo",
+        "u45-spx-h-hf-sp-full-hae-6deg-6mo",
+        "h45-ena-h-hf-sp-ram-hae-6deg-6mo",
+        "h45-ena-h-hf-sp-anti-hae-6deg-6mo",
+        "h45-ena-h-hf-sp-ram-hae-4deg-6mo",
+        "h45-ena-h-hf-sp-anti-hae-4deg-6mo",
+        "h90-ena-h-hf-sp-ram-hae-6deg-6mo",
+        "h90-ena-h-hf-sp-anti-hae-6deg-6mo",
+        "h90-ena-h-hf-sp-ram-hae-4deg-6mo",
+        "h90-ena-h-hf-sp-anti-hae-4deg-6mo",
+        "h45-ena-h-sf-nsp-full-hae-6deg-6mo",
+        "h45-ena-h-sf-nsp-ram-hae-6deg-6mo",
+        "h45-ena-h-sf-nsp-anti-hae-6deg-6mo",
+        "h45-ena-h-sf-nsp-full-hae-4deg-6mo",
+        "h45-ena-h-sf-nsp-ram-hae-4deg-6mo",
+        "h45-ena-h-sf-nsp-anti-hae-4deg-6mo",
+        "h90-ena-h-sf-nsp-full-hae-6deg-6mo",
+        "h90-ena-h-sf-nsp-ram-hae-6deg-6mo",
+        "h90-ena-h-sf-nsp-anti-hae-6deg-6mo",
+        "h90-ena-h-sf-nsp-full-hae-4deg-6mo",
+        "h90-ena-h-sf-nsp-ram-hae-4deg-6mo",
+        "h90-ena-h-sf-nsp-anti-hae-4deg-6mo",
+    ]
 
-    # release_path = Path(__file__).parent / "imap_maps_initial_release"
-    # for release_descriptor in MAPS_TO_RELEASE:
-    #     generated_path = generated_maps[release_descriptor]
-    #     science_file_path = ScienceFilePath(generated_path.name)
-    #
-    #     output_dir = release_path / science_file_path.instrument / science_file_path.data_level
-    #     output_dir.mkdir(parents=True, exist_ok=True)
-    #     shutil.copy(generated_path, output_dir)
+    release_path = Path(__file__).parent / "imap_maps_initial_release"
+    for release_descriptor in MAPS_TO_RELEASE:
+        generated_path = generated_maps[release_descriptor]
+        science_file_path = ScienceFilePath(generated_path.name)
+
+        new_name = (
+            "_".join(
+                [
+                    "imap",
+                    science_file_path.instrument,
+                    science_file_path.data_level,
+                    science_file_path.descriptor + "-INITIAL",
+                    science_file_path.start_date,
+                    science_file_path.version,
+                ]
+            )
+            + ".cdf"
+        )
+
+        output_dir = release_path / science_file_path.instrument / science_file_path.data_level
+        output_dir.mkdir(parents=True, exist_ok=True)
+        output_path = output_dir / new_name
+
+        with CDF(str(output_path), masterpath=str(generated_path), readonly=False) as cdf:
+            cdf.attrs["Logical_file_id"] = output_path.stem
